7019b9d7d3e10f9b1ce479b9531a68d399abd3b2
Tan-dat2001 <nguyentandat23122001@gmail.com>
2023-06-16 05:49:32 +0700

    change class NewAPI, create new package dto and class NewDTO

--- src/main/java/com/ntdstore/api/NewAPI.java
+++ src/main/java/com/ntdstore/api/NewAPI.java
@@ -1,13 +1,19 @@
 package com.ntdstore.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import com.ntdstore.dto.NewDTO;
+
+@Controller
 public class NewAPI {
-	
-	@GetMapping("/test")
-	public String testAPI() {
-		return "success";
+
+	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	@ResponseBody
+	public NewDTO createNew(@RequestBody NewDTO model) {
+		return model;
 	}
 }
