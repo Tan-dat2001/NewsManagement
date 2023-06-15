d3d331be61591d78a09e6fa8b01a7bbcbae67a06
Tan-dat2001 <nguyentandat23122001@gmail.com>
2023-06-16 05:29:26 +0700

    Tao project spring boot viet api web service

---
+++ src/main/java/com/ntdstore/api/NewAPI.java
@@ -0,0 +1,13 @@
+package com.ntdstore.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewAPI {
+	
+	@GetMapping("/test")
+	public String testAPI() {
+		return "success";
+	}
+}
